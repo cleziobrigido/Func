a731af6e1290d4e50fd373af5f909674e1ca2977
Clezio Brigido de Oliveira <69702466+cleziobrigido@users.noreply.github.com>
2023-06-13 10:52:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var numero1 = [3, 5, 7, 9, 11];
+var numero2 = [4, 6, 8, 10, 12];
+var numero3 = [];
+for(var i = 0; i < 4; i++){
+    numero3[i] = numero1[i] + numero2[i];
+}
+console.log(numero3);
